913a09df11ac3a4471897eb7c94a666b25c1a4f9
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-02-16 23:07:43 +0900

    Initial commit

---
+++ src/main/java/com/codestates/ToDoList/ToDoService.java
@@ -0,0 +1,41 @@
+package com.codestates.ToDoList;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ToDoService {
+    private final ToDoRepository toDoRepository;
+
+    public List<ToDoEntity> getList(){
+        return this.toDoRepository.findAll();
+    }
+
+    public void create(String title){
+        ToDoEntity toDoEntity = new ToDoEntity();
+        toDoEntity.setTitle(title);
+
+        this.toDoRepository.save(toDoEntity);
+    }
+
+    @Transactional
+    public void delete(Integer id){
+        ToDoEntity toDoEntity = toDoRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Item not found. id="+id));
+        this.toDoRepository.delete(toDoEntity);
+    }
+
+    @Transactional
+    public void update(Integer id, String title){
+        ToDoEntity toDoEntity = toDoRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Item not found. id="+id));
+        toDoEntity.setTitle(title);
+        this.toDoRepository.save(toDoEntity);
+    }
+
+
+}
